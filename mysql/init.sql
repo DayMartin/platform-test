eff65ff4786c40d0b90823248f9feb2a33642262
DayMartin <negociosdinah@gmail.com>
2024-05-17 04:24:04 -0300

    feat: database

---
+++ mysql/init.sql
@@ -0,0 +1,4 @@
+use dbrotas;
+
+CREATE TABLE IF NOT EXISTS `people`(`id` int NOT NULL AUTO_INCREMENT, `username` varchar(255) NOT NULL, `password` varchar(255) NOT NULL, PRIMARY KEY(`id`));
+-- CREATE TABLE IF NOT EXISTS `pontos`(`id` int NOT NULL AUTO_INCREMENT, `date_time` varchar(255) NOT NULL, `latitude` varchar(255) NOT NULL, `longitude` varchar(255) NOT NULL, PRIMARY KEY(`id`));
